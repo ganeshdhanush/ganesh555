8d0917d1d33b07774e6f9b7097891460d10703e0
ganeshdhanush <ganeshdhanush555@gmail.com>
2021-12-19 19:03:04 +0530

    Ganesh Task

---
+++ src/org/cts/Ganesh555.java
@@ -0,0 +1,15 @@
+package org.cts;
+
+public class Ganesh555 {
+public void sum() {
+int a,b,c;
+a=10;
+b=20;
+c=a+b;
+System.out.println(c);
+}
+public static void main(String[] args) {
+	Ganesh555 g = new Ganesh555();
+	g.sum();
+}
+}
